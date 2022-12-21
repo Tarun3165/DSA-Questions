caed6432c0a6694f4cf8dd7fbf78b0c873fbe7c7
Tarun Singh Rawat <rawattarun1997@gmail.com>
2022-12-21 13:59:21 +0530

    remove node

--- Linked List/ Remove Nth Node From End of List.js
+++ Linked List/ Remove Nth Node From End of List.js
@@ -1,5 +1,3 @@
-
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -12,49 +10,68 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {  //method 1  using length
-    let temp=head;
-    let count=1;
-    while(temp.next!=null){
-       count++;
-       temp=temp.next;
-    }
-    console.log(count)
-    temp=head;
-    let k=count-n;
-   
-    if(k==0){
-        return head.next
-    }
-    for(let i=1;i<k;i++){
-        temp=temp.next;
-    }
+var removeNthFromEnd = function (head, n) {
+  //method 1  using length
+  let temp = head;
+  let count = 1;
+  while (temp.next != null) {
+    count++;
+    temp = temp.next;
+  }
+  console.log(count);
+  temp = head;
+  let k = count - n;
 
-    temp.next=temp.next.next;
-    return head;
+  if (k == 0) {
+    return head.next;
+  }
+  for (let i = 1; i < k; i++) {
+    temp = temp.next;
+  }
+
+  temp.next = temp.next.next;
+  return head;
 };
 
+var removeNthFromEnd = function (head, n) {
+  //method 2  using two pointer
+  let temp = head;
+  let prev = head;
+  let pprev = head;
+  let p = false;
 
-var removeNthFromEnd = function(head, n) {  //method 2  using two pointer
-    let temp=head;
-    let prev=head;
-    let pprev=head;
-    let p=false;
+  for (let i = 1; i < n; i++) {
+    temp = temp.next;
+  }
+  while (temp.next != null) {
+    p = true;
+    temp = temp.next;
+    pprev = prev;
+    prev = prev.next;
+  }
+  pprev.next = prev.next;
+  if (p == false) {
+    return head.next;
+  }
+  return head;
+};
 
+var removeNthFromEnd = function (head, n) {
+  let start = new ListNode(-1, null);
+  start.next = head;
+  let temp = start;
+  let prev = head;
+  let pprev = start;
 
-   for(let i=1;i<n;i++){
-       
-       temp=temp.next;
-   }
-   while( temp.next!=null ){
-       p=true
-       temp=temp.next;
-       pprev=prev;
-       prev=prev.next;
-   }
-    pprev.next=prev.next;
-    if(p==false){
-        return head.next
-    }
-    return head;
+  for (let i = 1; i <= n; i++) {
+    temp = temp.next;
+  }
+  while (temp.next != null) { 
+    temp = temp.next;
+    pprev = prev;
+    prev = prev.next;
+  }
+  pprev.next = prev.next;
+
+  return start.next;
 };
